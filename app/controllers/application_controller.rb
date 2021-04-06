0fd5ec8b20806fdcbe37edbba578198ebf1268db
stacy <stacydn23@gmail.com>
2021-04-05 22:18:53 -0400

    commit tree 29962e9319478a5c27186e6b500e56f4b9e3824e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :is_loging? #this mean I can call it in any views
+  before_action :redirect_if_not, except: [:home]
+  before_action :is_loging?, only: :home
+
+  def home
+  end
+
+  def is_loging?
+    !!current_user
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def redirect_if_not
+    redirect_to root_path unless is_loging?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+  resources :users, only: [:show,:new, :create]
+
+  get '/signin' => 'sessions#signin', as: 'signin_session'
+  post '/signin' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: 'logout_session'
+
+  resources :attractions
+
+  post 'rides/new' => 'rides#create', as: 'new_ride'
 end
